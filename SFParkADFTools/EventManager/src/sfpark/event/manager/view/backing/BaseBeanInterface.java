3eed868f3437e3102f5ec5c6c75b7abd15d86f27
Mohammed Shaik Hussain Ali <mohammed.shaik.hussain.ali@oracle.com>
2011-08-10 18:36:15 -0700

    Calendar Manager Phase V - cleaning event calendar

--- SFParkADFTools/EventManager/src/sfpark/event/manager/view/backing/BaseBeanInterface.java
+++
@@ -1,5 +0,0 @@
-package sfpark.event.manager.view.backing;
-
-public interface BaseBeanInterface {
-    public void clearPageFlowScopeCache();
-}
--- SFParkADFTools/EventManager/src/sfpark/event/manager/view/backing/util/dialog/DialogBeanCallback.java
+++
@@ -1,7 +0,0 @@
-package sfpark.event.manager.view.backing.util.dialog;
-
-public interface DialogBeanCallback {
-    public void okButtonHandler();
-
-    public void cancelButtonHandler();
-}
