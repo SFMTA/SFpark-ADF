58f45da97cfbd6ec3f44c8a40213ce12efebbb55
Mohammed Shaik Hussain Ali <mohammed.shaik.hussain.ali@oracle.com>
2011-08-18 14:49:00 -0700

    Rate Change Manager Phase VII - Groundwork for model (area type pick)

---
+++ SFParkADFTools/Model/src/sfpark/adf/tools/model/data/helper/PMDistrictAreaType.java
@@ -0,0 +1,55 @@
+package sfpark.adf.tools.model.data.helper;
+
+public enum PMDistrictAreaType {
+    PILOT("Pilot", "Pilot"),
+    CONTROL("Control", "Control");
+
+    // ++++++++++++++++++++++++++++++++++++++++
+    // ++++++++++++++++++++++++++++++++++++++++
+    // ++++++++++++++++++++++++++++++++++++++++
+
+    public static PMDistrictAreaType extract(String areaTypeString) {
+        for (PMDistrictAreaType PMDistrictAreaType : values()) {
+            if (PMDistrictAreaType.getStringForTable().equalsIgnoreCase(areaTypeString)) {
+                return PMDistrictAreaType;
+            }
+        }
+
+        // Should never reach here
+        // Added this to avoid compilation errors
+        return PILOT;
+    }
+
+    // ++++++++++++++++++++++++++++++++++++++++
+    // ++++++++++++++++++++++++++++++++++++++++
+    // ++++++++++++++++++++++++++++++++++++++++
+
+    public boolean isPilot() {
+        return (this == PILOT);
+    }
+
+    public boolean isControl() {
+        return (this == CONTROL);
+    }
+
+    // ++++++++++++++++++++++++++++++++++++++++
+    // ++++++++++++++++++++++++++++++++++++++++
+    // ++++++++++++++++++++++++++++++++++++++++
+
+    private final String StringForTable;
+    private final String StringForDisplay;
+
+    private PMDistrictAreaType(String StringForTable,
+                               String StringForDisplay) {
+        this.StringForTable = StringForTable;
+        this.StringForDisplay = StringForDisplay;
+    }
+
+    public String getStringForTable() {
+        return StringForTable;
+    }
+
+    public String getStringForDisplay() {
+        return StringForDisplay;
+    }
+}
