1c17e59e866e0bd695bdbbe817ac76bc3c08b014
Mohammed Shaik Hussain Ali <mohammed.shaik.hussain.ali@oracle.com>
2011-09-01 17:10:28 -0700

    Cleaning up the Operation Status by adding the new class for CSS

---
+++ SFParkADFTools/Utilities/src/sfpark/adf/tools/utilities/constants/CSSClasses.java
@@ -0,0 +1,15 @@
+package sfpark.adf.tools.utilities.constants;
+
+public final class CSSClasses {
+
+    /**
+     * To avoid instantiation
+     */
+    private CSSClasses() {
+        super();
+    }
+
+    public static final String INLINE_MESSAGE_SUCCESS = "inlineMessageSuccess";
+    public static final String INLINE_MESSAGE_FAILURE = "inlineMessageFailure";
+
+}
