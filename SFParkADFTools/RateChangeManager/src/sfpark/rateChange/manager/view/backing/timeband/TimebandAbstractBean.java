5f43e66f50d5fae34400a93fd0a4000b838a1907
Mohammed Shaik Hussain Ali <mohammed.shaik.hussain.ali@oracle.com>
2011-10-20 17:59:25 -0700

    Rate Change Manager - Block Time Bands Phase XI (finalising the UI)

---
+++ SFParkADFTools/RateChangeManager/src/sfpark/rateChange/manager/view/backing/timeband/TimebandAbstractBean.java
@@ -0,0 +1,53 @@
+package sfpark.rateChange.manager.view.backing.timeband;
+
+import sfpark.rateChange.manager.application.key.PageFlowScopeKey;
+import sfpark.rateChange.manager.view.backing.BaseBean;
+import sfpark.rateChange.manager.view.helper.BlockTimeBandsWrapper;
+import sfpark.rateChange.manager.view.provider.DMLOperationsProvider;
+
+public abstract class TimebandAbstractBean extends BaseBean {
+    protected TimebandAbstractBean() {
+        super();
+    }
+
+    // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    // PUBLIC METHODS
+
+    public BlockTimeBandsWrapper getBlockTimeBandsWrapper() {
+        BlockTimeBandsWrapper wrapper =
+            (BlockTimeBandsWrapper)getPageFlowScopeValue(PageFlowScopeKey.BLOCK_TIME_BANDS_WRAPPER.getKey());
+
+        if (wrapper == null) {
+            wrapper =
+                    DMLOperationsProvider.INSTANCE.getNewBlockTimeBandsWrapper(getBlockID());
+            setPageFlowScopeValue(PageFlowScopeKey.BLOCK_TIME_BANDS_WRAPPER.getKey(),
+                                  wrapper);
+        }
+
+        return wrapper;
+    }
+
+    // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    // HELPER METHODS
+
+    protected void clearPageFlowScopeCache() {
+        removePageFlowScopeValue(PageFlowScopeKey.BLOCK_TIME_BANDS_WRAPPER.getKey());
+        removePageFlowScopeValue(PageFlowScopeKey.ADD_BLOCK_TIME_BANDS_DTO_LIST.getKey());
+        removePageFlowScopeValue(PageFlowScopeKey.TIMEBAND_PICKER_CHOSEN_LIST.getKey());
+    }
+
+    protected String getBlockID() {
+        String blockID =
+            (String)getPageFlowScopeValue(PageFlowScopeKey.BLOCK_ID.getKey());
+
+        if (blockID == null) {
+            blockID = "0";
+        }
+
+        return blockID;
+    }
+}
